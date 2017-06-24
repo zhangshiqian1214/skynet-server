db56557eacaa574e92948e23414b4f7b9041a416
kuzhu1990 <zhangshiqian1214@163.com>
2017-06-24 18:13:14 +0800

    fix service base

--- lualib/service_base.lua
+++ lualib/service_base.lua
@@ -1,7 +1,14 @@
 local skynet = require "skynet"
 
-local service = {}
+local service_base = {}
+
+service_base.name = nil
+service_base.modules = {}
+service_base.command = require "command_base"
+service_base.is_agent = false
+
+local command = service_base.command
 
 
 
-return service
+return service_base
